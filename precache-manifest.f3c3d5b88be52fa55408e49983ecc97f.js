11635b0894019799d0c91db0efef0390945b761d
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2020-05-02 19:59:05 -0400

    Updates

---
+++ precache-manifest.f3c3d5b88be52fa55408e49983ecc97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b6796b5ad44e13c0ba19fa648de56c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1307d85b55e2a4226103",
+    "url": "/Portfolio/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "601b169937ddc7fd3fa9",
+    "url": "/Portfolio/static/js/2.55a8be4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.55a8be4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1307d85b55e2a4226103",
+    "url": "/Portfolio/static/js/main.148f3bcf.chunk.js"
+  },
+  {
+    "revision": "abf4e20d5f7252ff3a3a",
+    "url": "/Portfolio/static/js/runtime-main.c05ec79d.js"
+  }
+]);
